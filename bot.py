b05c314fadb2c243f1563ced1d0fdd5bfdc9f8ee
Little-pure-light <purelighthappy@gmail.com>
2025-10-11 02:59:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
     
     
